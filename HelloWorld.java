136949062b13f5960c2f418d1735a9b4f30d93ea
Rod-S <rodsanati@gmail.com>
2021-05-16 23:03:38 -0500

    complete syntax, types, and variables

---
+++ HelloWorld.java
@@ -0,0 +1,33 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+//        int myFavoriteNumber = 13;
+//        System.out.println("myFavoriteNumber = " + myFavoriteNumber);
+//        String myString = "this is a string variable";
+//        System.out.println("myString + " + myString);
+//        float myNumber = (float) 3.14;
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        rewrite with shorthand
+//        int x = 4;
+//        x = x + 5;
+
+//        int x = 4;
+//        x += 5;
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+
+    }
+}
